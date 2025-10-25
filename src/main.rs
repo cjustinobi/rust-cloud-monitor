7f1863f4e7097b1a1b98d95a166493884faa8055
Justin Obi <cjustinobi@gmail.com>
2025-10-25 22:41:01 +0100

    feat: create metric route

--- src/main.rs
+++ src/main.rs
@@ -8,9 +8,11 @@ use axum::{
 async fn main() {
     // Create the router with /metrics endpoint
     let app = Router::new()
-        .route("/metrics", get(metrics_handler));
+        .route("/metrics", get(metrics_handler))
+        .route("/", get(root_handler));
 
     // Define the address to bind to
+    
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000")
         .await
         .unwrap();
@@ -25,6 +27,10 @@ async fn main() {
 }
 
 // Handler function for /metrics endpoint
-async fn metrics_handler() -> &'static str {
+async fn metrics_handler() -> & 'static str {
     "Hello Metrics"
+}
+
+async fn root_handler() -> &'static str {
+    "Hello, World!"
 }
